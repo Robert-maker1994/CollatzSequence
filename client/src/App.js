0a33be20c1919bff2ed97b8e65524d13a73631a3
Robert <robertpaullawson@gmail.com>
2021-09-08 19:03:42 +0100

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -19,9 +19,9 @@ function App() {
       //I've put an async on the then function to await for the response, otherwise it just response with an empty body
       .then(async (response) => {
         const body = await response.json();
-        console.log(body)
+        console.log(body);
         setanswerWithrecursion(body.WithRecursion);
-        setanswerWithoutrecursion(body.WithoutRecursion)
+        setanswerWithoutrecursion(body.WithoutRecursion);
       })
       .catch((err) => console.log(err));
   };
@@ -35,19 +35,26 @@ function App() {
       </div>
       {data ? <h1>The Collatz sequence of {data} is:</h1> : null}
       <div className="answer-container">
-     {answerWithrecursion ? <p>Answers with Recurrsion: </p> : null}
-      <div className="answer-container"> {answerWithrecursion
-          ? answerWithrecursion.map((i) => {
-              return <p key={i}>{i}</p>;
-            })
-          : null}
-          </div> 
-        <div className="answer-container">  {answerWithoutrecursion ? <p>Answers without Recurrsion: </p> : null}
-        {answerWithoutrecursion
-          ? answerWithoutrecursion.map((i) => {
-              return <p key={i}>{i}</p>;
-            })
-          : null} </div>
+        {/* Mapping the Function with Recurrsion answers */}
+        {answerWithrecursion ? <p>Answers with Recurrsion: </p> : null}
+        <div className="answer-container">
+          {" "}
+          {answerWithrecursion
+            ? answerWithrecursion.map((i) => {
+                return <p key={i}>{i}</p>;
+              })
+            : null}
+        </div>
+        <div className="answer-container">
+              {/* Mapping the Function without Recurrsion  */}
+
+          {answerWithoutrecursion ? <p>Answers without Recurrsion: </p> : null}
+          {answerWithoutrecursion
+            ? answerWithoutrecursion.map((i) => {
+                return <p key={i}>{i}</p>;
+              })
+            : null}{" "}
+        </div>
       </div>
     </div>
   );
