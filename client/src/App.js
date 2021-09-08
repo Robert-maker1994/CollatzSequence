4d3967d108c81c0f5dee17f200095a9f19faa048
Robert <robertpaullawson@gmail.com>
2021-09-08 19:02:04 +0100

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [answerWithrecursion, setanswerWithrecursion] = React.useState();
+  const [answerWithoutrecursion, setanswerWithoutrecursion] = React.useState();
 
-  React.useEffect(() => {
-    fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-  }, []);
+  const submit = async () => {
+    const init = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ data }),
+    };
+    await fetch("/api", init)
+      //I've put an async on the then function to await for the response, otherwise it just response with an empty body
+      .then(async (response) => {
+        const body = await response.json();
+        console.log(body)
+        setanswerWithrecursion(body.WithRecursion);
+        setanswerWithoutrecursion(body.WithoutRecursion)
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <div className="container">
+        <h1> Please enter a number</h1>
+        <input type="number" onChange={(e) => setData(e.target.value)} />
+        <button onClick={submit}>button</button>
+      </div>
+      {data ? <h1>The Collatz sequence of {data} is:</h1> : null}
+      <div className="answer-container">
+     {answerWithrecursion ? <p>Answers with Recurrsion: </p> : null}
+      <div className="answer-container"> {answerWithrecursion
+          ? answerWithrecursion.map((i) => {
+              return <p key={i}>{i}</p>;
+            })
+          : null}
+          </div> 
+        <div className="answer-container">  {answerWithoutrecursion ? <p>Answers without Recurrsion: </p> : null}
+        {answerWithoutrecursion
+          ? answerWithoutrecursion.map((i) => {
+              return <p key={i}>{i}</p>;
+            })
+          : null} </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,58 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+//Activating Cors after server 500 error
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("");
+  next();
+});
+
+// The RecursiveFunction answer
+const RecursiveFunction = (num) => {
+  let results = [];
+
+  num % 2 === 0 ? results.push(num / 2) : results.push(num * 3 + 1);
+
+  const actualResult = results[results.length - 1];
+
+  if (actualResult != 1) {
+    results = [...results, ...RecursiveFunction(actualResult)];
+  }
+  return results;
+};
+
+// The nonRecursive answer
+const nonRecursive = (n) => {
+  let results = [];
+  let count;
+  while (n > 1) {
+    n % 2 === 0 ? results.push((n = n / 2)) : results.push((n = n * 3 + 1));
+    count++;
+  }
+  return results;
+};
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "hello from the server " });
+});
+app.post("/api", (req, res) => {
+  const { data } = req.body;
+
+  const answerWithRecursion = RecursiveFunction(data);
+  const answerwithoutRecursion = nonRecursive(data);
+
+  return res.json({
+    WithRecursion: answerWithRecursion,
+    WithoutRecursion: answerwithoutRecursion,
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
